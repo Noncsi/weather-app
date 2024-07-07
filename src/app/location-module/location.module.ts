76f16b36b4f464cde7d34bb17030666b2e1e278f
Noncsi <lengyel.noemi96@gmail.com>
2024-07-07 21:14:22 +0200

    fix matinput issue

--- src/app/location-module/location.module.ts
+++ src/app/location-module/location.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { LocationComponent } from './components/location/location.component';
 
 @NgModule({
   declarations: [LocationComponent],
-  imports: [MatFormFieldModule],
+  imports: [MatFormFieldModule, MatInputModule],
   exports: [LocationComponent],
 })
 export class LocationModule {}
